7d5e39de23f1f496aab0388d20ca726f1b3bc5d7
Om Sharma <133844586+OmSharma42306@users.noreply.github.com>
2024-12-28 17:16:32 +0530

    Update projects.ts
    added new_project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -9,15 +9,15 @@ export const projects = [
   {
     title: "Video Calling App",
     description: "Real-time video calling application with WebRTC integration and signaling server.",
-    techStack: ["React", "Node.js", "WebSockets", "WebRTC", "Vercel", "Render"],
+    techStack: ["React", "Node.js", "TypeScript","WebSockets", "WebRTC", "Vercel", "Render"],
     liveLink: "https://t.co/ta2BXnL92n",
     repoLink: "https://t.co/MXV3kBeX8X"
   },
   {
-    title: "Project XYZ",
-    description: "Coming soon...",
-    techStack: ["TBD"],
-    liveLink: "",
-    repoLink: ""
+    title: "SimplyShare ",
+    description: "Real Time File Sharing Application with Implemented Webrtc and Websockets,DataChannels.",
+    techStack: ["React","Node.js","TypeScript","WebSockets", "WebRTC", "Vercel", "Render"],
+    liveLink: "https://t.co/JSMAIsopCt",
+    repoLink: "https://github.com/OmSharma42306/SimplyShare"
   }
 ];
